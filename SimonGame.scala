39718e0991ae116fc2b5d7f38b4ba2de464cf32c
lminy <laurent.miny@hotmail.com>
2016-04-07 16:20:05 +0200

    Adding sound + Refactoring

--- SimonGame.scala
+++ SimonGame.scala
@@ -1,51 +1,84 @@
+// scalac -classpath .;*.jar SimonGame.scala
+// scala -classpath .;*.jar SimonGame
 
 import scala.util.Random
 import scala.util.{Try, Success, Failure}
 import scala.language.postfixOps
+import java.io._
+
+// To play MP3 files
+import javazoom.jl.player.Player
 
 case class Button(color: String)
 
+// Interaction - VIEW
+object ButtonsManager{
+
+	def askButton(): Button = {
+		val color = scala.io.StdIn.readLine()
+		playSound("coins.mp3")
+		Button(color)
+	}
+
+	def showSequence(sequence: List[Button]){
+		for(button <- sequence){
+			println(button)
+			playSound("coins.mp3")
+		}
+	}
+
+	def playSound(sound: String) = {
+		val file = new File(sound)
+		val in = new FileInputStream(file.getAbsoluteFile)
+		val input = new BufferedInputStream(in)
+		val player = new Player(input) // jl1.0.jar (JLayer Mp3 library) http://www.javazoom.net/javalayer/sources.html
+		player.play()
+		/*
+		while(true){
+			player.play(1)
+		}*/
+	}
+}
+
+class SimonGame{
+
+	val redButton = Button("red")
+	val blueButton =  Button("blue")
+	val yellowButton = Button("yellow")
+	val greenButton = Button("green")
+
+	val buttons:List[Button] = List(redButton, blueButton, yellowButton, greenButton)
+
+	val infiniteSequence = buttonsGen
+
+	def start():Int = play(1) // Start the game
+
+	//Retourne le score
+	def play(size: Int): Int = {
+		val sequence = infiniteSequence.take(size).toList
+		ButtonsManager.showSequence(sequence)
+		completeSequence(sequence) match {
+			case true => play(size + 1)
+			case false => size - 1
+		}
+	}
+
+	def completeSequence(sequence: List[Button]): Boolean = sequence match {
+		case Nil => true
+		case x::xs if ButtonsManager.askButton() == x => completeSequence(xs)
+		case _ => false
+	}
+
+	def buttonsGen: Stream[Button] = {
+		val rnd = new scala.util.Random
+		buttons(rnd.nextInt(buttons length)) #:: buttonsGen
+	}
+}
+
 object SimonGame{
 	def main(args: Array[String]){
-		val redButton = Button("red")
-		val blueButton =  Button("blue")
-		val yellowButton = Button("yellow")
-		val greenButton = Button("green")
-
-		val buttons:List[Button] = List(redButton, blueButton, yellowButton, greenButton)
-
-		def buttonsGen: Stream[Button] = {
-			val rnd = new scala.util.Random
-			buttons(rnd.nextInt(buttons length)) #:: buttonsGen
-		}
-
-		def showSequence(sequence: List[Button]){
-			for(button <- sequence){
-				println(button)
-			}
-		}
-
-		def askButton(): Button = Button(scala.io.StdIn.readLine())
-
-		val infiniteSequence = buttonsGen
-
-		//Retourne le score
-		def play(size: Int): Int = {
-			val sequence = infiniteSequence.take(size).toList
-			showSequence(sequence)
-			completeSequence(sequence) match {
-				case true => play(size + 1)
-				case false => size - 1
-			}
-		}
-
-		def completeSequence(sequence: List[Button]): Boolean = sequence match {
-			case Nil => true
-			case x::xs if askButton() == x => completeSequence(xs)
-			case _ => false
-		}
-
-		val score = play(1) // Start the game
-		println(s"Résultat : $score")
+		val game = new SimonGame()
+		val score = game.start()
+		println(s"Result : $score")
 	}
 }
